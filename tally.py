20c68ba41ebf9d718dec86c2c8c4b4324e7bfd6f
Ben Adida <ben@BigDaddy.local>
2009-11-04 15:24:05 -0800

    fixed tally for multiple data locations

--- tally.py
+++ tally.py
@@ -2,9 +2,15 @@
 The tallying of the R tables, no verification otherwise
 
 Usage:
-python tally.py <DATA_PATH>
+python tally.py <QUESTION_ID> <DATA_PATH> <DATA_PATH_2> <DATA_PATH_3> ... 
 
-data path should NOT have a trailing slash
+QUESTION_ID is the question_id from electionspec.xml
+data paths should NOT have a trailing slash
+
+More than one data path because we are tallying up multiple wards at the same time
+
+The reason for specifying the question_num is that some questions are split among multiple wards, others not.
+
 """
 
 # core imports
@@ -13,49 +19,56 @@ import base, data, filenames
 
 import tallydata
 
+# correcting the argv base.DATA_PATH
+QUESTION_ID = sys.argv[1]
+DATA_PATHS = sys.argv[2:]  
+
+# for default election params
+base.DATA_PATH = DATA_PATHS[0]
+
 # use the meeting1 data structures
 from electionparams import *
 
 # import just the R tables
-r_tables_xml = base.file_in_dir(base.DATA_PATH, filenames.MEETING_THREE_OUT, 'Meeting Three Out')
-r_tables = data.parse_r_tables(r_tables_xml)
+# there could be a few given the multiple data_paths
+r_tables_xml = [base.file_in_dir(data_path, filenames.MEETING_THREE_OUT, 'Meeting Three Out') for data_path in DATA_PATHS]
+r_tables_list = [data.parse_r_tables(r_tables) for r_tables in r_tables_xml]
 
 print "ok now tallying\n\n"
 
 # the list of partitions, each of which is a list of the max number of answers each question allows
 partition_map = election.partition_map_choices
 
-# indexed by "p_id/q_id", where each value is an array of ballots of the appropriate type
-BALLOTS = {}
-
-for question in election.spec.questions:
-  BALLOTS[question.id] = []
+# we only tally per question now, so BALLOTS is just an array
+BALLOTS = []
   
 # go through each partition
-for p_id, r_table in r_tables.iteritems():
-  for row_id, row in r_table.rows.iteritems():
-    # split the result among questions for this partition, according to partition map
-    split_result = r_table.get_permutations_by_row_id(row_id, partition_map[p_id])
+for r_tables in r_tables_list:
+  for p_id, r_table in r_tables.iteritems():
+    for row_id, row in r_table.rows.iteritems():
+      # split the result among questions for this partition, according to partition map
+      split_result = r_table.get_permutations_by_row_id(row_id, partition_map[p_id])
     
-    # go through the questions
-    for q_num, question in enumerate(election.spec.questions_by_partition[p_id]):
-      # index 0 because there is only one permutation field in this table,
-      # but it's returned as a list, so we select the first and only one,
-      # then select the specific question number
-      raw_answer = split_result[0][q_num]
+      # go through the questions
+      for q_num, question in enumerate(election.spec.questions_by_partition[p_id]):
+        # skip over the questions we're not counting
+        if question.id != QUESTION_ID:
+          continue
+        
+        # index 0 because there is only one permutation field in this table,
+        # but it's returned as a list, so we select the first and only one,
+        # then select the specific question number
+        raw_answer = split_result[0][q_num]
       
-      # instantiate the right ballot type
-      ballot = tallydata.BALLOTS_BY_TYPE[question.type_answer_choice](raw_answer)
+        # instantiate the right ballot type
+        ballot = tallydata.BALLOTS_BY_TYPE[question.type_answer_choice](raw_answer)
       
-      BALLOTS[question.id].append(ballot)
+        BALLOTS.append(ballot)
 
-# now tally
-TALLIES = {}
+# now tally, only one tally
+TALLY = BALLOTS[0].tally(election.spec.questions_by_id[QUESTION_ID], BALLOTS)
 
-RESULTS = ""
-for q_id in sorted(BALLOTS.keys()):
-  TALLIES[q_id] = BALLOTS[q_id][0].tally(election.spec.questions_by_id[q_id], BALLOTS[q_id])
-  RESULTS += "Question %s: %s\n" % (q_id, TALLIES[q_id])
+RESULT = "Question %s: %s\n" % (QUESTION_ID, TALLY)
 
 def tally(output_stream):
   
@@ -66,7 +79,7 @@ Tally
 
 %s
 
-""" % (election.spec.id, len(r_tables[0].rows), RESULTS))
+""" % (election.spec.id, len(r_tables[0].rows), RESULT))
 
 if __name__ == '__main__':
   tally(sys.stdout)
